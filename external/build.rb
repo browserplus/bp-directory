917ed146ef7ce1f2d1f55746907a86fb238b4faa
Gordon Durand <gordon_durand@yahoo.com>
2011-04-01 15:01:03 -0700

    target 10.4 or 10.5

--- external/build.rb
+++ external/build.rb
@@ -2,6 +2,14 @@
 
 require "./bakery/ports/bakery"
 
+# XXX when 10.4 support dropped, get rid of this logic
+if ARGV.length == 1 && ARGV[0] == "osx10.4"
+  puts '*** DOING OSX10.4 BUILD **'
+  ENV['BP_OSX_TARGET'] = '10.4'
+else
+  ENV['BP_OSX_TARGET'] = ''
+end
+
 topDir = File.dirname(File.expand_path(__FILE__));
 $order = {
   :output_dir => File.join(topDir, "dist"),
